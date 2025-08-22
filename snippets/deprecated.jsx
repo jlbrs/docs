366bb76ccd3a07e257adf2ce98b630ceaaec2161
Jonas Lerebours <>
2025-08-22 18:40:35 +0200

    styling

--- snippets/deprecated.jsx
+++ snippets/deprecated.jsx
@@ -5,7 +5,7 @@ export const Deprecated = (props) => {
     <span className="px-1 py-0.5 rounded-md text-[0.65rem] border border-amber-500/20 text-amber-900 bg-amber-50/50 dark:border-amber-500/30 dark:bg-amber-500/10">
       Deprecated
     </span>} 
-    <span className="text-gray-400">
+    <span className="deprecated text-gray-400">
       {props.children}
     </span>
     </>
